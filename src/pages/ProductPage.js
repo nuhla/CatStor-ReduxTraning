f26ed4e1c7cd0877277a05ae8ce45ce2079db64f
nuhla <nuhlamasri@gmail.com>
2020-01-29 07:45:12 +0200

    add comments to the product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -4,17 +4,28 @@ import { getById } from '../api/products';
 export default class ProductPage extends React.Component {
   constructor(props) {
     super(props);
+    //------------------------------------//
+    //--- Define Our Needed States -------//
+    //------------------------------------//
     this.state = {
       loading: true,
+      quantuty: 0,
       product: {}
     };
   }
 
   componentDidMount() {
+    //-------------------------------------------------------------------//
+    //------ getting the id of the component from its props.params-------//
+    //-------------------------------------------------------------------//
     const id = this.props.match.params.id;
-
+    //-------------------------------------------------------------------//
+    //--------- get all information for this id from data stor ----------//
+    //-------------------------------------------------------------------//
     getById(id).then(product => {
-      console.log(product);
+      //-----------------------------------------------------------------------------------------------------------------------------//
+      //------ put the value of this object in Ouer State Object to force the component to re-render with the proper info -----------//
+      //-----------------------------------------------------------------------------------------------------------------------------//
       this.setState({
         product: product,
         loading: false
@@ -22,10 +33,27 @@ export default class ProductPage extends React.Component {
     });
   }
 
+  handelQuantityChange(e) {
+    //---------------------------------------------------------//
+    //---- getting the value form the input by handling--------//
+    //--------------- input onChange Method -------------------//
+    //---------------------------------------------------------//
+    this.setState({
+      quantuty: e.target.value
+    });
+  }
+
   render() {
+    //-------------------------------------------------------------------------------//
+    //------ if the data is not ready yet so return looding to the interface --------//
+    //-------------------------------------------------------------------------------//
     if (this.state.loading) {
       return 'loading ....';
     }
+    //----------------------------------------------------------------------------------//
+    //------------- When the data is ready to be shown then we have to return ----------//
+    //------------------------- the real component content -----------------------------//
+    //----------------------------------------------------------------------------------//
     return (
       <div>
         <br />
@@ -38,7 +66,10 @@ export default class ProductPage extends React.Component {
             <p className="card-text">{this.state.product.description}</p>
             <p className="card-text"> Price :{this.state.product.price} $</p>
 
-            <input type="number" />
+            <input
+              type="number"
+              onChange={this.handelQuantityChange.bind(this)}
+            />
             <br />
             <br />
             <button className="btn btn-primary"> Add to Cart</button>
